1ac86fdaee7ac764d367bb68904ed9c059290ff9
saiganeshk15 <saiganeshk15@gmail.com>
2023-10-10 20:59:15 +0530

    auth keys hidden

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-API_KEY: "IzaSyAbZHx_gVLorTqbtaekr4L6MmNeQ5ZDXZI",
-AUTH_DOMAIN : "registration-website-2023-24.firebaseapp.com",
-PROJECT_ID : "registration-website-2023-24",
-STORAGE_BUCKET : "registration-website-2023-24.appspot.com",
-MESSAGE_SENDER_ID : "937257506621",
-APP_ID : "1:937257506621:web:e5afebecc5923ddf0a1754",
-MESSUREMENT_ID : "G-4NZ0D089ZH"
-}

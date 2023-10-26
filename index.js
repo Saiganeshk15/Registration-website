decd3717f3ca07c0308871067381a59178cee21c
saiganeshk15 <saiganeshk15@gmail.com>
2023-10-26 13:45:45 +0530

    deleted files

--- index.js
+++
@@ -1,7 +0,0 @@
-var dropdownOptions = document.getElementById("dropdownOptions");
-var arrow = document.querySelector(".arrow");
-
-function toggleDropdown() {
-   dropdownOptions.style.display = dropdownOptions.style.display === "block" ? "none" : "block";
-   arrow.style.transform = arrow.style.transform === "rotate(180deg)" ? "rotate(0deg)" : "rotate(180deg) ";
-}
